c58482f6f65e7b5bb7f386b5c006b82e4bc4e945
adetunji <adekunleadetunjiwilson@gmail.com>
2023-10-27 18:55:51 +0100

    populated the api folder for task 3

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,2 +1,34 @@
 #!/usr/bin/python3
 """This is the module that controls the main flask application"""
+
+from models import storage
+from api.v1.views import app_views
+from flask import Flask
+from flask import jsonify
+from flask import make_response
+import os
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+
+
+@app.teardown_appcontext
+def teardown(exception):
+    """close connection to the database"""
+    storage.close()
+
+
+@app.errorhandler(404)
+def error_404(error):
+    """return a 404 json response"""
+    return make_response(jsonify({"error": "Not found"}), 404)
+
+
+if __name__ == "__main__":
+    host = os.getenv("HBNB_API_HOST")
+    if not host:
+        host = "0.0.0.0"
+    port = os.getenv("HBNB_API_PORT")
+    if not port:
+        port = 5000
+    app.run(host=host, port=port, threaded=True)
