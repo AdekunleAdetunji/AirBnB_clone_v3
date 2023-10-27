860e58e782dd1239dcbf936c12fb541bdd47a87f
Olawole Olawale Martins <olawalemart@gmail.com>
2023-10-27 17:03:56 +0100

    added get storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -33,6 +33,27 @@ class FileStorage:
                     new_dict[key] = value
             return new_dict
         return self.__objects
+    
+    def get(self, cls, id):
+        """returns a single item"""
+        if cls not in classes.values():
+            return None
+        all_items = self.all(cls).values()
+        for item in all_items:
+            if item.id == id:
+                return item
+        return None
+
+    def count(self, cls=None):
+        """count number of items in storage or number of specific class"""
+        count = 0
+
+        if not cls:
+            count = len(self.all().values())
+        else:
+            if cls in classes.values():
+                count = len(self.all(cls).values())
+        return count
 
     def new(self, obj):
         """sets in __objects the obj with key <obj class name>.id"""
