cc55c3fe61f66c736376d5f2cc9d924ff0df9252
Olawole Olawale Martins <olawalemart@gmail.com>
2023-10-28 12:53:57 +0100

    edited end points

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -12,7 +12,7 @@ from models.place import Place
 from models.user import User
 
 
-@app_views.route("/api/v1/cities/<city_id>/places", methods=["GET"],
+@app_views.route("/cities/<city_id>/places", methods=["GET"],
                  strict_slashes=False)
 def get_places(city_id):
     """gets all places in a city"""
@@ -23,17 +23,17 @@ def get_places(city_id):
     return make_response(jsonify(places), 200)
 
 
-@app_views.route("/api/v1/places/<place_id>", methods=["GET"],
+@app_views.route("/places/<place_id>", methods=["GET"],
                  strict_slashes=False)
 def get_place(place_id):
     """gets a single place item"""
     place = storage.get(Place, place_id)
     if not place:
         abort(404)
-    return make_response(jsonify(place), 200)
+    return make_response(jsonify(place.to_dict()), 200)
 
 
-@app_views.route("/api/v1/places/<place_id>", methods=["DELETE"],
+@app_views.route("/places/<place_id>", methods=["DELETE"],
                  strict_slashes=False)
 def del_place(place_id):
     """deletes a place item"""
@@ -45,7 +45,7 @@ def del_place(place_id):
     return make_response(jsonify({}), 200)
 
 
-@app_views.route("/api/v1/cities/<city_id>/places", methods=["POST"],
+@app_views.route("/cities/<city_id>/places", methods=["POST"],
                  strict_slashes=False)
 def post_place(city_id):
     """adds a new place"""
@@ -68,7 +68,7 @@ def post_place(city_id):
     return make_response(jsonify(place), 201)
 
 
-@app_views.route("/api/v1/places/<place_id>", methods=["PUT"],
+@app_views.route("/places/<place_id>", methods=["PUT"],
                  strict_slashes=False)
 def update_place(place_id):
     """updates a place item"""
