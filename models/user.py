b8cf49178ed09ce7783124843e6dd463ae2b7783
Olawole Olawale Martins <olawalemart@gmail.com>
2023-10-30 22:25:27 +0100

    added hashing of password

--- models/user.py
+++ models/user.py
@@ -6,6 +6,7 @@ from os import getenv
 import sqlalchemy
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+import hashlib
 
 
 class User(BaseModel, Base):
@@ -27,3 +28,11 @@ class User(BaseModel, Base):
     def __init__(self, *args, **kwargs):
         """initializes user"""
         super().__init__(*args, **kwargs)
+
+    def __setattr__(self, k, v):
+        """sets hashed pasword"""
+        if k == "password":
+            # encode to convert to byte string
+            byte_value = v.encode()
+            hashed_value = hashlib.md5(byte_value).hexdigest()
+        super().__setattr__(k, hashed_value)
